c5a9b88df0940c623cf22795708f61e66054e125
Jason Yip <jason.yip@uconn.edu>
2020-05-15 19:26:48 -0400

    Added bootstrap and created simple table for unit data

--- fire-emblem-stats/src/components/6-BindingBlade.vue
+++ fire-emblem-stats/src/components/6-BindingBlade.vue
@@ -2,13 +2,13 @@
     <div class = "6">
         <h1> {{msg}} </h1>
         <h1> Skeleton </h1>
-        <p>
         <ul class="nav">
             <router-link to="/6-BindingBlade/Roy">Roy</router-link>
             <router-link to="/6-BindingBlade/Fir">Fir</router-link>
         </ul>
-        </p>
         <router-view></router-view>
+        <h2> Roy </h2>
+        <b-table :items="Roy"></b-table>
     </div>
 </template>
 
@@ -17,13 +17,22 @@ export default {
     name: '6',
     data() {
         return {
-            msg: 'Homepage for Fire Emblem 6: The Binding Blade'
+            msg: 'Homepage for Fire Emblem 6: The Binding Blade',
+            Roy: [
+                {Level: 1, HP: 18, STR: 5, SKL: 5, SPD: 7, LCK: 7, DEF: 5, RES: 0},
+                {Level: 2, HP: 18.8, STR: 5.4, SKL: 5.5, SPD: 7.4, LCK: 7.6, DEF: 5.25, RES: 0.3}
+            ],
+            Fir: [
+                {Level: 1, HP: 18, STR: 5, SKL: 5, SPD: 7, LCK: 7, DEF: 5, RES: 0},
+                {Level: 2, HP: 18.8, STR: 5.4, SKL: 5.5, SPD: 7.4, LCK: 7.6, DEF: 5.25, RES: 0.3}
+            ]
         }
     },
     methods: {
         goBack() {
             window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
-        }
+        },
+
     },
     computed: {
 
