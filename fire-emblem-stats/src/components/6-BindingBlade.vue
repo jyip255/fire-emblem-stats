e11bc6ccf7f1dd63f04593597549ec7e56af9c55
Jason Yip <jason.yip@uconn.edu>
2020-05-15 21:38:32 -0400

    Added an actual dynamic stat table with fixed rounding, and added an extra character for testing

--- fire-emblem-stats/src/components/6-BindingBlade.vue
+++ fire-emblem-stats/src/components/6-BindingBlade.vue
@@ -5,9 +5,33 @@
         <b-dropdown id="dropdown" text="Choose a unit" class="m-2">
             <b-dropdown-item @click="changeTo(Roy, 'Roy')">Roy</b-dropdown-item>
             <b-dropdown-item @click="changeTo(Fir, 'Fir')">Fir</b-dropdown-item>
+            <b-dropdown-item @click="changeTo(Miredy, 'Miredy')">Miredy</b-dropdown-item>
         </b-dropdown>
         <h2> {{ target.title }} </h2>
-        <b-table :items="target.info"></b-table>
+        <b-container>
+            <b-row>
+                <b-col>Level</b-col>
+                <b-col>HP</b-col>
+                <b-col>S/M</b-col>
+                <b-col>Skl</b-col>
+                <b-col>Spd</b-col>
+                <b-col>Lck</b-col>
+                <b-col>Def</b-col>
+                <b-col>Res</b-col>
+            </b-row>
+            <div v-for="(entry, index) in target2" v-bind:key="index">
+            <b-row>
+                <b-col> {{entry.level}} </b-col>
+                <b-col> {{entry.hp}} </b-col>
+                <b-col> {{entry.str}} </b-col>
+                <b-col> {{entry.skl}} </b-col>
+                <b-col> {{entry.spd}} </b-col>
+                <b-col> {{entry.lck}} </b-col>
+                <b-col> {{entry.def}} </b-col>
+                <b-col> {{entry.res}} </b-col>
+            </b-row>
+            </div>
+        </b-container>
     </div>
 </template>
 
@@ -19,30 +43,80 @@ export default {
             msg: 'Homepage for Fire Emblem 6: The Binding Blade',
             target: {
                 title: 'No Unit Selected',
-                info: ''
+                info: '',
+                level: '',
+                hp: '',
+                str: '',
+                skl: '',
+                spd: '',
+                lck: '',
+                def: '',
+                res: ''
             },
-            Roy: 
-                [
-                {Level: 1, HP: 18, STR: 5, SKL: 5, SPD: 7, LCK: 7, DEF: 5, RES: 0},
-                {Level: 2, HP: 18.8, STR: 5.4, SKL: 5.5, SPD: 7.4, LCK: 7.6, DEF: 5.25, RES: 0.3}
-            ],
-            Fir: [
-                {Level: 1, HP: 19, STR: 5, SKL: 5, SPD: 7, LCK: 7, DEF: 5, RES: 0},
-                {Level: 2, HP: 18.8, STR: 5.4, SKL: 5.5, SPD: 7.4, LCK: 7.6, DEF: 5.25, RES: 0.3}
-            ]
+            target2: [],
+            Roy: {
+                name: 'Roy',
+                base: [
+                    1, 18, 5, 5, 7, 7, 5, 0
+                ],
+                growths: [
+                    1, .80, .40, .50, .40, .6, .25, .30
+                ],
+                stats: [
+                ]
+            },
+            Fir: {
+                name: 'Fir',
+                base: [
+                    1, 19, 6, 9, 10, 3, 3, 1
+                ],
+                growths: [
+                    1, .75, .25, .50, .55, .50, .15, .20
+                ],
+                stats: [
+                ]
+            },
+            Miredy: {
+                name: 'Miredy',
+                base: [
+                    10, 30, 12, 11, 10, 5, 13, 3
+                ],
+                growths: [
+                    1, .75, .50, .50, .45, .25, .20, .05
+                ],
+                stats: [
+                ]
+            }
         }
     },
     methods: {
         goBack() {
             window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
         },
+        calculateStats: function(unit) {
+            this.target2 = [];
+            var i;
+            var cap = 41 - unit.base[0]
+            for (i = 0; i < cap; i++) {
+                this.target2.push({
+                    level: ((unit.base[0] + (unit.growths[0] * i))),
+                    hp: Math.round(((unit.base[1] + (unit.growths[1] * i)))*100)/100,
+                    str: Math.round(((unit.base[2] + (unit.growths[2] * i)))*100)/100,
+                    skl: Math.round(((unit.base[3] + (unit.growths[3] * i)))*100)/100,
+                    spd: Math.round(((unit.base[4] + (unit.growths[4] * i)))*100)/100,
+                    lck: Math.round(((unit.base[5] + (unit.growths[5] * i)))*100)/100,
+                    def: Math.round(((unit.base[6] + (unit.growths[6] * i)))*100)/100,
+                    res: Math.round(((unit.base[7] + (unit.growths[7] * i)))*100)/100
+            })
+            }
+        },
         changeTo(e, name) {
             this.target.info = e
             this.target.title = name
-        }
+            this.calculateStats(e)
+        },
     },
     computed: {
-
     }
 }
 </script>
