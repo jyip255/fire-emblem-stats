aa8150dd291afbd34e3d69ae4b4493da277e62b2
Jason Yip <jason.yip@uconn.edu>
2020-05-15 20:13:05 -0400

    added events and dynamic tabling by selecting a dropdown option

--- fire-emblem-stats/src/components/6-BindingBlade.vue
+++ fire-emblem-stats/src/components/6-BindingBlade.vue
@@ -7,8 +7,12 @@
             <router-link to="/6-BindingBlade/Fir">Fir</router-link>
         </ul>
         <router-view></router-view>
-        <h2> Roy </h2>
-        <b-table :items="Roy"></b-table>
+        <b-dropdown id="dropdown" text="Choose a unit" class="m-2">
+            <b-dropdown-item @click="changeTo(Roy, 'Roy')">Roy</b-dropdown-item>
+            <b-dropdown-item @click="changeTo(Fir, 'Fir')">Fir</b-dropdown-item>
+        </b-dropdown>
+        <h2> {{ target.title }} </h2>
+        <b-table :items="target.info"></b-table>
     </div>
 </template>
 
@@ -18,12 +22,17 @@ export default {
     data() {
         return {
             msg: 'Homepage for Fire Emblem 6: The Binding Blade',
-            Roy: [
+            target: {
+                title: 'No',
+                info: ''
+            },
+            Roy: 
+                [
                 {Level: 1, HP: 18, STR: 5, SKL: 5, SPD: 7, LCK: 7, DEF: 5, RES: 0},
                 {Level: 2, HP: 18.8, STR: 5.4, SKL: 5.5, SPD: 7.4, LCK: 7.6, DEF: 5.25, RES: 0.3}
             ],
             Fir: [
-                {Level: 1, HP: 18, STR: 5, SKL: 5, SPD: 7, LCK: 7, DEF: 5, RES: 0},
+                {Level: 1, HP: 19, STR: 5, SKL: 5, SPD: 7, LCK: 7, DEF: 5, RES: 0},
                 {Level: 2, HP: 18.8, STR: 5.4, SKL: 5.5, SPD: 7.4, LCK: 7.6, DEF: 5.25, RES: 0.3}
             ]
         }
@@ -32,7 +41,10 @@ export default {
         goBack() {
             window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
         },
-
+        changeTo(e, name) {
+            this.target.info = e
+            this.target.title = name
+        }
     },
     computed: {
 
